57d747b30b182607e24aa9ef91f716c1cb6cdec4
Miguel Ceriani <miguel.ceriani@gmail.com>
2018-08-28 18:36:50 +0100

    Fix default configuration file and missing dependency

--- tests/tests.py
+++ tests/tests.py
@@ -14,32 +14,32 @@ class MediatorTests(unittest.TestCase):
     config = None
     statsURI = None
     clearStatsURI = None
-    
+
     @classmethod
     def setUpClass(self):
 
         # read config file
-        try:        
+        try:
             self.config = os.environ["MEDIATORTEST"]
         except KeyError:
             logging.error("You must specify a configuration file through the variable $MEDIATORTEST")
-            sys.exit()            
+            sys.exit()
         yf = open(self.config, "r")
-        ym = yaml.load(yf)        
+        ym = yaml.load(yf)
         self.mediatorBaseURI = ym["mediator"]["baseUri"]
         self.audioclipSearchPath = ym["mediator"]["routes"]["audioclipSearch"]["path"]
         self.audioclipSearchArgs = ym["mediator"]["routes"]["audioclipSearch"]["args"]
         self.audioclipShowPath = ym["mediator"]["routes"]["audioclipShow"]["path"]
         self.audioclipShowArgs = ym["mediator"]["routes"]["audioclipShow"]["args"]
         self.audioclipAnalysePath = ym["mediator"]["routes"]["audioclipAnalyse"]["path"]
-        self.audioclipAnalyseArgs = ym["mediator"]["routes"]["audioclipAnalyse"]["args"]    
+        self.audioclipAnalyseArgs = ym["mediator"]["routes"]["audioclipAnalyse"]["args"]
         self.collectionSearchPath = ym["mediator"]["routes"]["collectionSearch"]["path"]
         self.collectionSearchArgs = ym["mediator"]["routes"]["collectionSearch"]["args"]
 
         # close file
         yf.close()
 
-        
+
     def test_00_successful_audioclip_search(self):
 
         # request configuration
@@ -56,12 +56,12 @@ class MediatorTests(unittest.TestCase):
         # make the request
         r = requests.get(self.mediatorBaseURI + self.audioclipSearchPath + params)
         msg = json.loads(r.text)
-        
+
         # check that returns 200
         self.assertEqual(200, r.status_code)
         self.assertEqual("ok", msg["status"])
 
-        
+
     def test_01_successful_audioclip_show(self):
 
         # request configuration
@@ -79,7 +79,7 @@ class MediatorTests(unittest.TestCase):
         uri = self.mediatorBaseURI + self.audioclipShowPath % "418106"
         r = requests.get(uri + params)
         msg = json.loads(r.text)
-        
+
         # check that returns 200
         self.assertEqual(200, r.status_code)
         self.assertEqual("ok", msg["status"])
@@ -101,7 +101,7 @@ class MediatorTests(unittest.TestCase):
         # make the request
         r = requests.get(self.mediatorBaseURI + self.collectionSearchPath + params)
         msg = json.loads(r.text)
-        
+
         # check that returns 200
         self.assertEqual(200, r.status_code)
         self.assertEqual("ok", msg["status"])
@@ -124,12 +124,12 @@ class MediatorTests(unittest.TestCase):
         mid = self.audioclipAnalysePath % "1498355"
         r = requests.get(self.mediatorBaseURI + mid + params)
         msg = json.loads(r.text)
-        
+
         # check that returns 200
         self.assertEqual(200, r.status_code)
         self.assertEqual("ok", msg["status"])
 
-        
-                
+
+
 if __name__ == "__main__":
     unittest.main(verbosity = 3)
