36bbccb0ed2dbffbc04db0754fe2311ad81891d7
Miguel Ceriani <miguel.ceriani@gmail.com>
2018-09-17 14:24:20 +0100

    Add ordering of framed JSON-LD result

--- lib/QueryUtils.py
+++ lib/QueryUtils.py
@@ -30,7 +30,10 @@ class QueryUtils:
                 elif isinstance(field, rdflib.term.BNode):
                     triple_string += " _:%s " % field
                 else:
-                    triple_string += " '%s' " % field.replace("'", "\\'")
+                    languageStr = ('@' + field.language) if field.language else ''
+                    datatypeStr = ('^^<' + field.datatype + '>') if field.datatype else ''
+                    valueStr = "'%s'" % field.replace("'", "\\'")
+                    triple_string += " " + valueStr + languageStr + datatypeStr + " "
             triples.append(triple_string)
 
         # return
@@ -49,6 +52,51 @@ class QueryUtils:
         # return
         return insertData
 
+    @staticmethod
+    def getOrderFromFrame(frame):
+        if not isinstance(frame, dict):
+            return None
+        newDict = {}
+        if "@order" in frame:
+            newDict["@order"] = frame["@order"]
+            del frame["@order"]
+        for key,value in frame.items():
+            subframe = QueryUtils.getOrderFromFrame(value)
+            if subframe is not None:
+                newDict[key] = subframe
+        return newDict if len(newDict) > 0 else None
+
+    @staticmethod
+    def executeOrderList(input, order):
+        input.sort(key=lambda item: item[order] if order in item else None)
+
+    @staticmethod
+    def executeOrder(input, order):
+        if not isinstance(order, dict):
+            return
+        if isinstance(input, dict):
+            for key, value in input.items():
+                if key in order:
+                    QueryUtils.executeOrder(value, order[key])
+        elif isinstance(input, list):
+            if "@order" in order:
+                QueryUtils.executeOrderList(input, order["@order"])
+            for item in input:
+                QueryUtils.executeOrder(item, order)
+
+    @staticmethod
+    def frameAndCompact(input, frame, context):
+        orderStruct = QueryUtils.getOrderFromFrame(frame)
+        framedResults = jsonld.frame(json.loads(input), frame)
+        compactedResults = jsonld.compact(framedResults, context)
+        if "@graph" in compactedResults:
+            results = compactedResults["@graph"]
+        else:
+            if "@context" in compactedResults:
+                del compactedResults["@context"]
+            results = compactedResults
+        QueryUtils.executeOrder(results, orderStruct)
+        return results
 
     @staticmethod
     def getJsonLD(queryResults, frame, context):
@@ -63,19 +111,18 @@ class QueryUtils:
                         t.append(URIRef(triple[field]["value"]))
                     elif triple[field]["type"] == "bnode":
                         t.append(BNode(triple[field]["value"]))
-                    else:
-                        t.append(Literal(triple[field]["value"]))
+                    elif triple[field]["type"] == "literal":
+                        if "xml:lang" in triple[field]:
+                            t.append(Literal(triple[field]["value"], lang=triple[field]["lang"]))
+                        elif "datatype" in triple[field]:
+                            type = URIRef(triple[field]["datatype"])
+                            t.append(Literal(triple[field]["value"], datatype=type))
+                        else:
+                            t.append(Literal(triple[field]["value"]))
                 g.add(t)
 
             # return
             jld = g.serialize(format="json-ld")
-            framedResults = jsonld.frame(json.loads(jld), frame)
-            compactedResults = jsonld.compact(framedResults, context)
-            if "@graph" in compactedResults:
-                return compactedResults["@graph"]
-            else:
-                if "@context" in compactedResults:
-                    del compactedResults["@context"]
-                return compactedResults
+            return QueryUtils.frameAndCompact(jld, frame, context)
         except:
             print(traceback.print_exc())
