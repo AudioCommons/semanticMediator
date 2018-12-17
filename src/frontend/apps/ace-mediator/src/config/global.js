c13d005f1b5a1ab10aad95e6cf100e8f53efcba5
Sasha Rudan <mprinc@gmail.com>
2018-12-17 13:16:34 +0000

    Updated flow names in config

--- src/frontend/apps/ace-mediator/src/config/global.js
+++ src/frontend/apps/ace-mediator/src/config/global.js
@@ -80,7 +80,7 @@ if (!globalSet.hasOwnProperty('puzzles')) {
             timeDivider: 1000000,
             showActionNamesonFlow: false,
             flowImages: [{
-                    name: 'search-sounds',
+                    name: 'searchForSounds',
                     title: 'Search Sounds',
                     imageUrl: "assets/images/flows/flow-search.jpg",
                     actions: [{
