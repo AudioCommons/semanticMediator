26454a2db2a93d3beb3f062c5c04f334146bd037
Sasha Rudan <mprinc@gmail.com>
2019-01-21 15:30:15 +0000

    Updated e2e tests

--- src/services/mediator/tests/cache-missing.sh
+++ src/services/mediator/tests/cache-missing.sh
@@ -1,5 +1,7 @@
-url=http://localhost:9027/audioclips/search?pattern=
-# url=https://m2.audiocommons.org/api/audioclips/search?pattern=
+# url=http://localhost:9027/audioclips/search?pattern=
+url=https://m2.audiocommons.org/api/audioclips/search?pattern=
+# cd /Users/sasha/Documents/data/development/QMUL/infrastructure-2/semanticMediator/src/services/mediator/tests/
+# sh ./cache-missing.sh
 ## declare an array variable
 declare -a arr=("dog" "cat" "fish" "bird" "love" "dance" "bamos" "ciao" "ptica" "privet" "food" "bamboo" "flower" "deep" "clear" "rain" "sun" "peace" "mission" "chance")
 
